75d82739468544e88313b957b8d13d817ad93ab1
MetaReal <mhershadi84@gmail.com>
2023-11-15 21:58:49 +0330

    Added token.h comments

--- heads/lexer/lexer.h
+++ heads/lexer/lexer.h
@@ -10,18 +10,21 @@
 
 struct __LEX_RES_T
 {
-    token_t *tokens; /**< list of tokens */
+    token_t *tokens;            /**< List of tokens */
 
-    illegal_char_t error; /**< illegal character error */
+    illegal_char_t error;       /**< Illegal character error */
 };
-typedef struct __LEX_RES_T lex_res_t; /**< result of the lex function */
+typedef struct __LEX_RES_T lex_res_t; /**< Result of the lex function */
 
 /**
  * \fn lex_res_t lex(const char *code)
- * Main function for converting raw code string into tokens
+ * Converts raw MetaReal code into the list of tokens.
  * \param code The raw MetaReal code
- * \return The result of analysis as a list of tokens.
- * If there was an IllegalCharacterError, The error is returned with a NULL tokens list.
+ * \return
+ * \pre
+ * The result of analysis as a list of tokens.
+ * If there was an illegal character error, the error is returned with a NULL tokens list.
+ * If there wan an expected character error, the error is returned with a NULL tokens list.
 */
 lex_res_t lex(const char *code);
 
--- heads/lexer/token.h
+++ heads/lexer/token.h
@@ -9,113 +9,141 @@
 
 enum __TOKEN_TYPES
 {
-    EOF_T,
+    EOF_T,              /**< End of file */
 
-    NEWLINE_T,
-    SEMICOLON_T,
+    NEWLINE_T,          /**< Newline */
+    SEMICOLON_T,        /**< Semicolon ; */
 
-    ID_T,
-    INT_T,
-    FLOAT_T,
-    IMAG_T,
-    CHAR_T,
-    STR_T,
+    ID_T,               /**< Identifier */
+    INT_T,              /**< Integer number */
+    FLOAT_T,            /**< Floating point number */
+    IMAG_T,             /**< Imaginary number */
+    CHAR_T,             /**< Character */
+    STR_T,              /**< String */
 
-    MUL_T,
-    DIV_T,
-    MOD_T,
-    QUOT_T,
-    POW_T,
+    MUL_T,              /**< Multiply * */
+    DIV_T,              /**< Divide / */
+    MOD_T,              /**< Modulo % */
+    QUOT_T,             /**< Quotient // */
+    POW_T,              /**< Power ** */
 
-    B_AND_T,
-    B_OR_T,
-    B_XOR_T,
-    LSHIFT_T,
-    RSHIFT_T,
+    B_AND_T,            /**< Binary and & */
+    B_OR_T,             /**< Binary or | */
+    B_XOR_T,            /**< Binary xor ^ */
+    LSHIFT_T,           /**< Left shift << */
+    RSHIFT_T,           /**< Right shift >> */
 
-    EQ_T,
-    NEQ_T,
-    EX_EQ_T,
-    EX_NEQ_T,
+    EQ_T,               /**< Equal == */
+    NEQ_T,              /**< Not equal != */
+    EX_EQ_T,            /**< Exact equal === */
+    EX_NEQ_T,           /**< Exact not equal !== */
 
-    LT_T,
-    GT_T,
-    LTE_T,
-    GTE_T,
+    LT_T,               /**< Less than < */
+    GT_T,               /**< Greater than > */
+    LTE_T,              /**< Less than or equal to <= */
+    GTE_T,              /**< Greater than or equal to >= */
 
-    INC_T,
-    DEC_T,
-    ASSIGN_T,
-    LINK_T,
+    INC_T,              /**< Increment ++ */
+    DEC_T,              /**< Decrement -- */
+    ASSIGN_T,           /**< Assign = */
+    LINK_T,             /**< Link -> */
 
-    ADD_EQ_T,
-    SUB_EQ_T,
-    MUL_EQ_T,
-    DIV_EQ_T,
-    MOD_EQ_T,
-    QUOT_EQ_T,
-    POW_EQ_T,
+    ADD_EQ_T,           /**< Add and store += */
+    SUB_EQ_T,           /**< Subtract and store -= */
+    MUL_EQ_T,           /**< Multiply and store *= */
+    DIV_EQ_T,           /**< Divide and store /= */
+    MOD_EQ_T,           /**< Modulo and store %= */
+    QUOT_EQ_T,          /**< Quotient and store //= */
+    POW_EQ_T,           /**< Power and store **= */
 
-    B_AND_EQ_T,
-    B_OR_EQ_T,
-    B_XOR_EQ_T,
-    LSHIFT_EQ_T,
-    RSHIFT_EQ_T,
+    B_AND_EQ_T,         /**< Binary and and store &= */
+    B_OR_EQ_T,          /**< Binary or and store |= */
+    B_XOR_EQ_T,         /**< Binary xor and store ^= */
+    LSHIFT_EQ_T,        /**< Left shift and store <<= */
+    RSHIFT_EQ_T,        /**< Right shift and store >>= */
 
-    LPAREN_T,
-    RPAREN_T,
+    LPAREN_T,           /**< Left parenthesis ( */
+    RPAREN_T,           /**< Right parenthesis ) */
 
-    LSQUARE_T,
-    RSQUARE_T,
+    LSQUARE_T,          /**< Left square bracket [ */
+    RSQUARE_T,          /**< Right square bracket ] */
 
-    COMMA_T,
+    COMMA_T,            /**< Comma , */
 
-    ADD_T,
-    SUB_T,
-    B_NOT_T,
+    ADD_T,              /**< Add + */
+    SUB_T,              /**< Subtract - */
+    B_NOT_T,            /**< Binary not ~ */
 
+    /**
+     * 'not' keyword
+     * Not !
+    */
     NOT_KT,
+    /**
+     * 'and' keyword
+     * And &&
+    */
     AND_KT,
+    /**
+     * 'or' keyword
+     * Or ||
+    */
     OR_KT,
 
-    VAR_KT,
+    VAR_KT,             /**< 'var' keyword */
 
-    CONST_KT,
+    CONST_KT,           /**< 'const' keyword */
 
-    NONE_KT,
-    TRUE_KT,
-    FALSE_KT,
+    NONE_KT,            /**< 'none' keyword */
+    TRUE_KT,            /**< 'true' keyword */
+    FALSE_KT,           /**< 'false' keyword */
 
-    INT_TT,
-    FLOAT_TT,
-    COMPLEX_TT,
+    INT_TT,             /**< 'int' datatype */
+    FLOAT_TT,           /**< 'float datatype */
+    COMPLEX_TT,         /**< 'complex' datatype */
 
-    BOOL_TT,
+    BOOL_TT,            /**< 'bool' datatype */
 
-    CHAR_TT,
-    STR_TT,
+    CHAR_TT,            /**< 'char' datatype */
+    STR_TT,             /**< 'str' datatype */
 
-    LIST_TT,
-    TUPLE_TT
-};
+    LIST_TT,            /**< 'list' datatype */
+    TUPLE_TT            /**< 'tuple' datatype */
+}; /**< Token types */
 
-#define TOKENS_LEN 67
+#define TOKENS_LEN 67 /**< Number of token types */
 
 struct __TOKEN_T
 {
-    uint8_t type;
+    uint8_t type;               /**< Type of the token (from TOKEN_TYPES enum) */
 
-    char *value;
-    uint64_t size;
+    char *value;                /**< Value of the token (NULL if the token is a symbol)*/
+    uint64_t size;              /**< Size of the value */
 
-    pos_t poss;
-    pos_t pose;
+    pos_t poss;                 /**< Starting position of the token in the MetaReal raw code */
+    pos_t pose;                 /**< Ending position of the token in the MetaReal raw code */
 };
-typedef struct __TOKEN_T token_t;
+typedef struct __TOKEN_T token_t; /**< Word equivalent in MetaReal translation process */
 
-extern const char *token_labels[TOKENS_LEN];
+extern const char *token_labels[TOKENS_LEN]; /**< Label of the tokens (for debugging) */
 
+/**
+ * \fn void tokens_free(token_t *tokens)
+ * \pre
+ * Gets the list of tokens and deallocates all the tokens inside it (not the tokens pointer itself).
+ * The tokens list should end with an EOF token.
+ * \param tokens The list of tokens
+*/
 void tokens_free(token_t *tokens);
+
+/**
+ * \fn void tokens_print(const token_t *tokens)
+ * \pre
+ * Prints the list of tokens in the stdout stream (for debugging).
+ * The tokens list should end with an EOF token.
+ * Each token is printed in a separate line.
+ * \param tokens The list of tokens
+*/
 void tokens_print(const token_t *tokens);
 
 #endif /* __MR_TOKEN__ */
